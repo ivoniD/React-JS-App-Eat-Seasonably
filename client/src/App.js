3aca140bad3d3d3dd3ba4f4a09fd1c6983cfb4fc
Ivon Dimitrova <ivon_y@abv.bg>
2023-03-26 22:07:12 +0300

    test

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,9 @@ import { Login } from "./components/Login/Login";
 import { Register } from "./components/Register/Register";
 import { Seasons } from "./components/Seasons/Seasons";
 import { Route, Routes } from 'react-router-dom';
-import { useNavigate } from "react-router-dom";
+
 import { Home } from "./components/Home/Home";
-import { Other } from "./components/Other";
+
 import {useEffect, useState} from 'react'
 import './App.css'
 
