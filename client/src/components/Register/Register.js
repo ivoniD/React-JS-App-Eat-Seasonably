04e5d6777242fe9fa03384f1d0463c625a294567
Ivon Dimitrova <ivon_y@abv.bg>
2023-04-14 12:54:27 +0300

    register validation

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -7,9 +7,42 @@ import { Link, useNavigate } from 'react-router-dom';
 
 
 export const Register = () => {
-  const [errorMessage, setErrorMessage] = useState(null)
+  // const [errorMessage, setErrorMessage] = useState(null)
   const { userLogin } = useContext(AuthContext)
   const navigate = useNavigate();
+  const [error, setError] = useState({
+    name: null,
+    email: null,
+    password: null
+  })
+
+  const validateName = (e) => {
+    const name = e.target.value;
+    let errorMsg = '';
+    if (name.length < 3) {
+      errorMsg = 'Name must be more that 3 characters.'
+    }
+    if (name.length > 25) {
+      errorMsg = 'Name must be shorter that 25 characters.'
+    }
+    setError(state => ({
+      ...state,
+      name: errorMsg
+    }))
+  }
+  const validateEmail = (e) => {
+    const email = e.target.value;
+    let errorMsg = '';
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if(!emailRegex.test(email)){
+      errorMsg = 'Email address must be valid.'
+    }
+    setError(state => ({
+      ...state,
+      email: errorMsg
+    }))
+  }
 
 
   const onSubmit = (e) => {
@@ -20,29 +53,25 @@ export const Register = () => {
     const password = formData.get('password')
     const rePass = formData.get('repeat-password')
 
-    // if(name === '' || email === '' || password === '' || rePass=== ''){
-    //   setErrorMessage('All fields are required!')
-    // }else(
-    //   setErrorMessage(null)
-    // )
-
-    // if(password !== rePass){
-    //     setErrorMessage("Passwords don't match")
-    // }else{
-    //   setErrorMessage(null)
-    // }
-    
-
-
+    if(password !== rePass){
+      setError(state => ({
+        ...state,
+        password: "Passwords don't match"
+      }))
+    }else{
+      setError(state => ({
+        ...state,
+        password: null
+      }))
       register(name, email, password)
       .then(authData => {
         userLogin(authData)
-        setErrorMessage('')
         navigate('/')
       })
+    }
+  
     
-
-
+    
   }
   return(
     <div className='home-cont'>
@@ -51,12 +80,15 @@ export const Register = () => {
         <h2 className="register">Register</h2>
         <label>
           <span className="register-text">Name</span>
-          <input type="text" name="name" />
+          <input type="text" name="name"  onBlur={validateName}/>
         </label>
+        {error.name && <span style={{ color: 'red', 'font-size': '20px' }}>{error.name}</span>}
         <label>
           <span className="register-text">Email</span>
-          <input type="email" name="email" />
+          <input type="email" name="email" onBlur={validateEmail}/>
+          
         </label>
+        {error.email && <span style={{ color: 'red', 'font-size': '20px' }}>{error.email}</span>}
         <label>
           <span className="register-text">Password</span>
           <input type="password" name="password" />
@@ -65,7 +97,7 @@ export const Register = () => {
           <span className="register-text">Repeat Password</span>
           <input type="password" name="repeat-password" />
         </label>
-        {errorMessage !== '' && <span className="error-msg">{errorMessage}</span>}
+        {error.password && <span style={{ color: 'red', 'font-size': '20px' }}>{error.password}</span>}
         <button type="submit" className="submit">
           Sign Up
         </button>
